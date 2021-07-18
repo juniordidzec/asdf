5bf0c6e80be1d0ceb5e5af20100f91d4287b79d9
Junior Didzec <junior_didzec@hotmail.com>
2021-07-17 23:33:28 -0300

    Change js

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@ const form = document.getElementById('form')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    alert('Hello devs!');
     let email= document.getElementById('email').value;
     let data = {
         email
     }
     let convertData = JSON.stringify(data);
-    console.log(data);
     localStorage.setItem('lead', convertData)
+    if (email) {
+        alert('E-mail cadastrado!');
+    }
 })
